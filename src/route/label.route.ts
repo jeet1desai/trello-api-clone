30566bf2aa9fa826f8fde83b0198d3ef9e19a828
jeet1desai <jeet.desai@tatvasoft.com>
2025-04-11 12:15:35 +0530

    added delete label api

--- src/route/label.route.ts
+++ src/route/label.route.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import userMiddleware from '../middleware/user.middleware';
-import { createLabelHandler } from '../controller/label.controller';
+import { createLabelHandler, deleteLabelHandler, updateLabelHandler } from '../controller/label.controller';
 
 const labelRouter = express.Router();
 
 labelRouter.use(userMiddleware);
 labelRouter.post('/create-label', createLabelHandler);
+labelRouter.put('/update-label/:id', updateLabelHandler);
+labelRouter.delete('/delete-label/:id', deleteLabelHandler);
 
 export default labelRouter;
