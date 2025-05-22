8eebfa680cc0415bc1cedcbddc4d99ab15539684
Dhruv21 <dhruvik.bhagiya@tatvasoft.com>
2025-05-22 10:47:40 +0530

    update and remove background API

--- src/route/status.route.ts
+++ src/route/status.route.ts
@@ -1,6 +1,12 @@
 import express from 'express';
 import userMiddleware from '../middleware/user.middleware';
-import { createStatusHandler, deleteStatusHandler, getStatusByBoardIdHandler, updateStatusHandler } from '../controller/status.controller';
+import {
+  createStatusHandler,
+  deleteStatusHandler,
+  getStatusByBoardIdHandler,
+  updateStatusHandler,
+  removeStatusBackgroundHandler,
+} from '../controller/status.controller';
 
 const statusRouter = express.Router();
 
@@ -9,5 +15,6 @@ statusRouter.post('/create-status', createStatusHandler);
 statusRouter.get('/get-status', getStatusByBoardIdHandler);
 statusRouter.put('/update-status', updateStatusHandler);
 statusRouter.delete('/delete-status/:id', deleteStatusHandler);
+statusRouter.delete('/remove-background-status/:statusId', removeStatusBackgroundHandler);
 
 export default statusRouter;
